529e8848d9becaea707811b4f7dd3ad300c22e4c
Pavithra-margg <pavithramargg@gmail.com>
2025-01-17 19:07:23 +0530

    JS

---
+++ JS/Intro/Task.js
@@ -0,0 +1,116 @@
+// IF STATEMENTS:
+
+// 1.Check even or  odd.
+
+// let number = parseInt(prompt("Enter a Number"));
+
+// if (number %2=== 0) {
+//     console.log(number +" is Even number")
+// } else {
+//     console.log(number +" is Add number")
+// }
+
+
+// 2.Check if a number is divisible by 5.
+
+// let five= parseInt(prompt("Enter a divisable by five"));
+
+// if (five % 5 == 0) {
+//     console.log(five + " is devisible by 5")
+// } else {
+//     console.log(five +" is not devisible by 5")
+// }
+
+
+// 3. Compare two numbers and print the larger one.
+
+// let Value_1 = parseInt(prompt("Enter a 1st Value"));
+// let Value_2 = parseInt(prompt("Enter a 2nd Value"));
+
+// if (Value_1 > Value_2) {
+//     console.log(Value_1 + " is greater than Value_2")
+    
+// } else if (Value_1 < Value_2) {
+//     console.log(Value_2 + " is greter than Value_1")
+    
+// } else if (Value_1 == Value_2) {
+//     console.log("Value_1 and Value_2 are Equal")
+
+// }else {
+//      console.log("Please enter the correct Value")
+// }
+
+
+// 4.Check if a string contains a specific substring.
+
+// let string = prompt("Enter a String");
+// let substring = prompt("Enter a Substring");
+
+// if (string.includes(substring)) {
+//   console.log("It is string contains a specific substring");
+// } else {
+//   console.log("It is not string contains a specific substring");
+// }
+
+
+// 5.Check if a number is between 1 and 10.
+
+// let number_1 = parseInt(prompt("Enter a Value"));
+
+// if (number_1>=0 & number_1<=10) {
+//     console.log("It is number between 1 and 10")
+// } else {
+//     console.log ("It is not number between 1 and 10")
+// }
+
+
+// 6. Check if a character is a vowel.
+
+
+// let Alpha = prompt("Enter a Vowels");
+// let vowels = ["a", "e", "i", "o", "u"];
+
+// if (Alpha in vowels) {
+//     console.log("It is vowels")
+// } else {
+//     console.log("It is not vowels")
+// }
+
+
+
+
+
+
+
+// 7.Check if a person is eligible to vote (age >= 18).
+// 8.Determine if a year is a leap year.
+// 9.Check if a given number is a multiple of 3 and 7.
+// 10.Check if a person is a teenager (age between 13 and 19).
+// 11.Find the largest of three numbers.
+// 12.Check if a number is divisible by both 5 and 11.
+// 13.Determine the grade based on marks.
+// 14.Check if a number is positive and even.
+// 15.Check if a number lies between 10 and 20.
+// 16.Determine if a string is empty.
+// 17.check the number is positive or negative
+// 18.check if a number is prime or not a prime number.
+// NESTED IF STATEMENTS TASK:
+
+// 1.Find the largest of three numbers.
+// 2..Check if a number is divisible by 2, 3, or both.
+// 3.Check if a number lies in the range 10-50.
+// 4.Check if a year is a leap year.
+// 5. Check if a number is positive, negative, or zero, and whether it is even or odd.
+// 6.check  if a number is positive, negative, or zero.
+// 7.check if a number is even or odd and within a 1 -10 range.
+// 8.Check if a person is eligible to vote by citizen.
+
+// Switch CASE TASK:
+
+// 1.Check a day as weekday or weekend.
+// 2.print a no of days in a month.
+// 3.print a entered input is vowels or consonants.
+// 4.grade by percentage.
+// 5.season based on month name.
+// 6.positive or negative
+// 7.Get input from user print if it is range between 1-50.
